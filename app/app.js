395c81c8adc60273bc3cefd0772084014d153736
opentlc-mgr <lilim@redhat.com>
2021-05-06 03:32:20 +0000

    updated

--- app/app.js
+++ app/app.js
@@ -187,6 +187,6 @@ function createOrder(req, res) {
 
 
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(8080, function () {
+  console.log('Example app listening on port 8080!');
 });
